bf78e9ce98ca8c26ed8259895ce7dfe194eb6d90
David Davis <david.ddavis@gmail.com>
2009-06-05 21:30:02 -0700

    display welcome banner via Rubbs#post_init
    The banner itself could be improved.
    
    Added test code to bottom of lib/rubbs.rb (ruby lib/rubbs) -- aids
    hackability.
    
    port 1560 -- Uncle Vinny's Pizza (did we want to run over IRC?)

--- TODO.md
+++ TODO.md
@@ -1,5 +1,4 @@
 TODO
 ====
 
-* Display welcome banner in Rubbs#post_init
 * Add command handler to Rubbs#receive_line
--- lib/rubbs.rb
+++ lib/rubbs.rb
@@ -4,10 +4,10 @@ require 'eventmachine'
 class Rubbs < EventMachine::Protocols::LineAndTextProtocol
 
   # Initiates a TCP socket acceptor on the specified IP address and port.
-  # 
+  #
   # ==== Parameters
   # ip<String>:: IP address. Required.
-  # port<Integer>:: Port number. Required. 
+  # port<Integer>:: Port number. Required.
   #
   def self.start(ip, port)
     EM.run {
@@ -15,4 +15,12 @@ class Rubbs < EventMachine::Protocols::LineAndTextProtocol
     }
   end
 
+  def post_init
+    send_data "Hello.  Welcome to my world.\n"
+  end
+
+end
+
+if $0 == __FILE__
+  Rubbs.start '127.0.0.1', 1560
 end
